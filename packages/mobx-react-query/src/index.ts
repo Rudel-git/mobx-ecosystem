57d6eb36ae14842f38f004b470bff37c0aa6b0b7
RudikS <rudik005@gmail.com>
2023-08-10 17:14:55 +0400

    mobx-react-query | add types

--- packages/mobx-react-query/src/index.ts
+++ packages/mobx-react-query/src/index.ts
@@ -2,3 +2,4 @@ export * from './mutation-service';
 export * from './infinite-query-service';
 export * from './query-service';
 export { configureMobxReactQuery } from './config';
+export * from './types';
