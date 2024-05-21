3bf2e56513ed43ca295ef79227c5aa4e206b10e9
RudikS-git <rudik005@gmail.com>
2024-05-21 18:36:16 +0400

    mobx-form | add autocomplete-field-service to index

--- packages/mobx-form/src/index.ts
+++ packages/mobx-form/src/index.ts
@@ -3,3 +3,4 @@ export * from './form-service';
 export * from './combined-form-field-service';
 export * from './configure-form';
 export * from './types';
+export * from './autocompete-field-service';
